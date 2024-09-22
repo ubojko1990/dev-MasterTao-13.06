d3c64fbd1fc49b807bdb427cddd98213c1e365e5
ubojko1990 <ubojko1990@gmail.com>
2024-09-22 10:14:25 +0300

    feat:banner done

--- build/assets/js/custom.js
+++ build/assets/js/custom.js
@@ -1,1 +1,35 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   
+  });
+
+  const name = "Toyota"
+  const lastName = "Aygo"
+  const old ="2005"
+  
+  console.log (name+ ""+ lastName + "" + old);
+
+
+  const user = {
+    name: "Macbook",
+    old: "2020", 
+    series: {
+      Air: {
+        model: '13.6',
+      },
+      Pro: {
+        model: '14'
+      }
+
+    }
+  }
+  console.log(user.series.Air.model);
+
