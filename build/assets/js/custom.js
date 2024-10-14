4276a1678aaa2ab9279358479aed64e00fc65e00
ubojko1990 <ubojko1990@gmail.com>
2024-10-14 18:25:05 +0300

    feat:swiper-template desctop amd mob version are done

--- build/assets/js/custom.js
+++ build/assets/js/custom.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('#banner-slider', {
     loop: true,
     pagination: {
       el: '.swiper-pagination',
@@ -24,27 +24,6 @@ const swiper = new Swiper('.swiper', {
 
   });
 
-  const name = "Toyota"
-  const lastName = "Aygo"
-  const old ="2005"
-  
-  console.log (name+ ""+ lastName + "" + old);
-
-
-  const user = {
-    name: "Macbook",
-    old: "2020", 
-    series: {
-      Air: {
-        model: '13.6',
-      },
-      Pro: {
-        model: '14'
-      }
-
-    }
-  }
-  console.log(user.series.Air.model);
 
 
   const typefile = document.querySelectorAll('.custom-file')
@@ -56,3 +35,99 @@ const swiper = new Swiper('.swiper', {
     })
   })
 
+  
+  const advantagesSlideLength = document.querySelectorAll('.advantages .swiper-slide').length
+
+  const swiperAdvantages = new Swiper('#advantages', {
+    // spaceBetween: 12,
+    loop: true,
+    simulateTouch: true,
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      enabled: true,
+    },
+    breakpoints: {
+      992: {
+        spaceBetween: 0,
+        loop: false,
+        slidesPerGroup: advantagesSlideLength,
+        simulateTouch: false,
+        pagination: {
+          enabled: false,
+          el: '.swiper-pagination',
+        }
+        
+      }
+    }
+
+  });
+
+  
+
+  const swiperSliderTemplate = new Swiper('.slider-template', {
+    speed: 400,
+    loop: true,
+    slidesPerView: 1,
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      enabled: true,
+    },
+    navigation: {
+      enabled: false,
+      nextEl: '.swiper-button-next-unique',
+      prevEl: '.swiper-button-prev-unique',
+        
+  },
+
+  breakpoints: {
+    767: {
+      slidesPerView: 1,
+      navigation: {
+        enabled: true,
+        nextEl: '.swiper-button-next-unique',
+        prevEl: '.swiper-button-prev-unique',
+          
+    },
+    pagination: {
+      enabled: true,
+    },
+    
+
+ },
+
+   861: {
+       slidesPerView: 2,
+       spaceBetween: 30,
+       navigation: {
+        enabled: true,
+        nextEl: '.swiper-button-next-unique',
+        prevEl: '.swiper-button-prev-unique',
+       },
+
+       pagination: {
+        enabled: false,
+      },
+          
+    },
+  
+
+    1252: {
+        slidesPerView: 3,
+        spaceBetween: 30, 
+        navigation: {
+          enabled: false,
+          nextEl: '.swiper-button-next-unique',
+          prevEl: '.swiper-button-prev-unique',
+            
+         },
+         pagination: {
+          enabled: false,
+        },
+       
+      }
+    }
+});
